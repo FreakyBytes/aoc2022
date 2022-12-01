f715abc51ce2270d5894ad7dd7bc2ca41f238a82
Martin Peters <martin@freakybytes.net>
2022-12-01 23:20:17 +0100

    prepared all days

--- aoc2022-02/src/main.rs
+++ aoc2022-02/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_name = std::env::args().nth(1).expect("No input file supplied!");
+    let reader = BufReader::new(File::open(file_name)?);
+    for line in reader.lines() {
+        let line = line?;
+        println!("{}", line);
+    }
+
+    Ok(())
 }
