61a310c7789301979d72ac32c0f05f756b079a87
Martin Peters <martin@freakybytes.net>
2022-12-03 13:51:42 +0100

    03-2 done

--- aoc2022-03/src/main.rs
+++ aoc2022-03/src/main.rs
@@ -1,9 +1,26 @@
+use itertools::Itertools;
 use std::{
     collections::HashSet,
     error::Error,
     fs::File,
     io::{BufRead, BufReader},
 };
+
+// #[derive(Debug)]
+// struct Rucksack {
+//     pub comp1: HashSet<char>,
+//     pub comp2: HashSet<char>,
+// }
+
+// impl Rucksack {
+//     pub fn new(line: String) -> Self {
+//         let (comp1, comp2) = line.split_at(line.len() / 2);
+//         Rucksack {
+//             comp1: HashSet::from_iter(comp1.chars()),
+//             comp2: HashSet::from_iter(comp2.chars()),
+//         }
+//     }
+// }
 
 fn score_from_char(input: &char) -> u32 {
     if input.is_ascii_lowercase() {
@@ -14,24 +31,55 @@ fn score_from_char(input: &char) -> u32 {
         panic!("'{}' is not an ascii character!", input);
     }
 }
+
+fn div_floor(a: usize, b: u32) -> u32 {
+    ((a as f64) / f64::from(b)).floor() as u32
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let file_name = std::env::args().nth(1).expect("No input file supplied!");
     let reader = BufReader::new(File::open(file_name)?);
 
-    let mut total_score: u32 = 0;
-    for line in reader.lines() {
-        let line = line?;
-        let (comp1, comp2) = line.split_at(line.len() / 2);
-        let comp1: HashSet<char> = HashSet::from_iter(comp1.chars());
-        let comp2: HashSet<char> = HashSet::from_iter(comp2.chars());
+    let groups = reader
+        .lines()
+        .into_iter()
+        .map(|line| HashSet::<char>::from_iter(line.unwrap().chars()))
+        .enumerate()
+        .group_by(|(idx, _)| div_floor(*idx, 3));
 
-        println!("{}:{:?} {}:{:?}", comp1.len(), comp1, comp2.len(), comp2);
-        if let Some(intersect) = comp1.intersection(&comp2).next() {
-            let score = score_from_char(intersect);
+    let mut total_score: u32 = 0;
+    for (group, rucks) in &groups {
+        let badge = rucks
+            .map(|(_, ruck)| ruck)
+            .reduce(|accum, item| -> HashSet<char> {
+                accum.intersection(&item).copied().collect()
+            });
+        // println!("{}: {:?}", group, badge);
+        if let Some(badge) = badge {
+            let badge = badge
+                .iter()
+                .next()
+                .ok_or_else(|| anyhow::Error::msg("No badge for you!"))?;
+
+            let score = score_from_char(badge);
             total_score += score;
-            println!(" -> {} {}", intersect, score);
+            println!("{} [{}] -> {}", group, badge, score);
         }
     }
+
+    // for line in reader.lines() {
+    //     let line = line?;
+    //     let (comp1, comp2) = line.split_at(line.len() / 2);
+    //     let comp1: HashSet<char> = HashSet::from_iter(comp1.chars());
+    //     let comp2: HashSet<char> = HashSet::from_iter(comp2.chars());
+
+    //     println!("{}:{:?} {}:{:?}", comp1.len(), comp1, comp2.len(), comp2);
+    //     if let Some(intersect) = comp1.intersection(&comp2).next() {
+    //         let score = score_from_char(intersect);
+    //         total_score += score;
+    //         println!(" -> {} {}", intersect, score);
+    //     }
+    // }
 
     println!("==> {}", total_score);
 
