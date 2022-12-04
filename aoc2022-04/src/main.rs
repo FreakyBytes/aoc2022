2c971b69964ecf0c53e815d10b7e9a2af42ed52b
Martin Peters <martin@freakybytes.net>
2022-12-04 15:49:10 +0100

    04-2 done

--- aoc2022-04/src/main.rs
+++ aoc2022-04/src/main.rs
@@ -1,11 +1,10 @@
 use anyhow::Context;
 use std::{
+    collections::HashSet,
     error::Error,
     fs::File,
     io::{BufRead, BufReader},
 };
-
-use itertools::Itertools;
 
 fn split_range(range: &str) -> Result<(u32, u32), anyhow::Error> {
     let (from, to) = range
@@ -19,7 +18,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file_name = std::env::args().nth(1).expect("No input file supplied!");
     let reader = BufReader::new(File::open(file_name)?);
 
-    let pairs_with_covered_range = reader
+    let overlapping_pairs = reader
         .lines()
         .into_iter()
         .filter_map(|l| l.ok())
@@ -29,15 +28,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         };
             (split_range(group1).unwrap(), split_range(group2).unwrap())
         })
+        // // fully overlapping ranges
+        // .filter(|((g1_from, g1_to), (g2_from, g2_to))| {
+        //     // group 1 includes 2
+        //     g1_from <= g2_from && g1_to >= g2_to
+        // // group 2 includes 1
+        // || g2_from <= g1_from && g2_to >= g1_to
+        // })
+        // partly overlapping ranges
         .filter(|((g1_from, g1_to), (g2_from, g2_to))| {
-            // group 1 includes 2
-            g1_from <= g2_from && g1_to >= g2_to
-        // group 2 includes 1
-        || g2_from <= g1_from && g2_to >= g1_to
+            let g1: HashSet<u32> = HashSet::from_iter((*g1_from)..=(*g1_to));
+            let g2: HashSet<u32> = HashSet::from_iter((*g2_from)..=(*g2_to));
+            g1.intersection(&g2).count() > 0
         })
         .count();
 
-    println!("Pairs with fully overlapping ranges: {pairs_with_covered_range}");
+    println!("Pairs with overlapping ranges: {overlapping_pairs}");
 
     Ok(())
 }
