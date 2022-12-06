26daae23e507338c9cd1a3c6f506fabd4b2f2f4e
Martin Peters <martin@freakybytes.net>
2022-12-06 06:26:51 +0100

    06 done

--- aoc2022-06/src/main.rs
+++ aoc2022-06/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use itertools::Itertools;
+use std::{
+    collections::HashSet,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_name = std::env::args().nth(1).expect("No input file supplied!");
+    let lines = BufReader::new(File::open(file_name)?)
+        .lines()
+        .into_iter()
+        .filter_map(|line| line.ok());
+
+    for line in lines {
+        print!("{line}");
+        let pos = line
+            .chars()
+            .tuple_windows::<(_, _, _, _)>()
+            .find_position(|(a, b, c, d)| a != b && a != c && a != d && b != c && b != d && c != d);
+        println!(
+            "start of packet => {:?}",
+            pos.map(|(p, chars)| (p + 4, chars))
+        );
+
+        for pos in 14..line.len() {
+            let window = &line[(pos - 14)..pos];
+            let unique_window: HashSet<char> = HashSet::from_iter(window.chars());
+            if unique_window.len() == 14 {
+                println!("start of message => {pos} {window}");
+                break;
+            }
+        }
+    }
+
+    Ok(())
 }
