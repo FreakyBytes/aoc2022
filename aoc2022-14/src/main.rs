83bc905cbaf46a7746337348be6321d5f094d3e8
Martin Peters <martin.peters@naval-architect.io>
2022-12-14 19:45:26 +0100

    14 prep

--- aoc2022-14/src/main.rs
+++ aoc2022-14/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use anyhow::{Error, Context};
+use itertools::Itertools;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_name = std::env::args().nth(1).expect("No input file supplied!");
+    let rock_paths = BufReader::new(File::open(file_name)?)
+        .lines()
+        .into_iter()
+        .filter_map(|line| line.ok())
+        .map(|line| {
+            line.split("->")
+            .map(|coord| coord.trim().split_once(",").ok_or_else(|| Error::msg(format!("Failed to parse coordinates: '{coord}'"))))
+            .map_ok(|(x, y)| -> Result<(i32, i32), Error> { Ok((x.parse::<i32>().context("Failed to parse x")?, y.parse::<i32>().context("Failed to parse y")?))})
+            .collect::<Result<Vec<(i32, i32)>, _>>()
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+<
+    Ok(())
 }
