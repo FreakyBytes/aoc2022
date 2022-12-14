fd525b72e9678b655fe212a357c4cb41841bfed3
Martin Peters <martin@freakybytes.net>
2022-12-14 23:34:07 +0100

    14-2 done

--- aoc2022-14/src/main.rs
+++ aoc2022-14/src/main.rs
@@ -1,11 +1,10 @@
 use std::{
-    cell::Cell,
     fs::File,
     io::{BufRead, BufReader},
 };
 
 use anyhow::{Context, Error};
-use itertools::Itertools;
+use itertools::{iproduct, Itertools};
 use ndarray::Array2;
 
 const START: (i32, i32) = (500, 0);
@@ -73,8 +72,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         })
         .collect::<Result<Vec<_>, Error>>()?;
 
-    // dbg!(&rock_paths);
-
     let (x_min, x_max, y_min, y_max) = rock_paths
         .iter()
         .map(|path| {
@@ -87,8 +84,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         })
         .fold(
             (
-                START.0 as i32,
-                START.0 as i32,
+                0,
+                1000,
+                // START.0 as i32,
+                // START.0 as i32,
                 START.1 as i32,
                 START.1 as i32,
             ),
@@ -104,7 +103,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     dbg!((x_min, x_max, y_min, y_max));
 
     let spawn = ((START.0 - x_min) as usize, (START.1 - y_min) as usize);
-    let grid_size = ((x_max - x_min) as usize + 1, (y_max - y_min + 1) as usize);
+    let grid_size = ((x_max - x_min) as usize + 1, (y_max - y_min + 3) as usize);
     let mut grid = Grid::default(grid_size);
 
     for path in rock_paths.iter() {
@@ -124,77 +123,86 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // grid[[(x - x_min) as usize, (y - y_min) as usize]] = GridState::Rock;
     }
-    grid[[spawn.0, spawn.1]] = GridState::Spawn;
+    // grid[[spawn.0, spawn.1]] = GridState::Spawn;
+    for x in 0..grid_size.0 {
+        grid[[x, grid_size.1 - 1]] = GridState::Rock;
+    }
+
     render_grid(&grid);
 
     let mut units: u32 = 0;
     let mut units_at_rest: u32 = 0;
     loop {
         let units_before = units;
-        if grid[[spawn.0, spawn.1 + 1]] != GridState::Air {
+        if grid[[spawn.0, spawn.1]] != GridState::Air {
+            println!("Units reached spawn!");
             break;
         } else {
             units += 1;
-            grid[[spawn.0, spawn.1 + 1]] = GridState::Sand(SandState::Moving);
+            grid[[spawn.0, spawn.1]] = GridState::Sand(SandState::Moving);
         }
 
-        // for col in grid.co
-        for y in (0..grid_size.1).rev() {
-            for x in (0..grid_size.0) {
-                match grid[[x, y]] {
-                    // sand falls out of area
-                    GridState::Sand(SandState::Moving) if y >= grid_size.1 - 1 => {
-                        grid[[x, y]] = GridState::Air;
-                        units -= 1;
-                    }
-                    // falling straight down
-                    GridState::Sand(SandState::Moving) if grid[[x, y + 1]] == GridState::Air => {
-                        grid[[x, y]] = GridState::Air;
-                        grid[[x, y + 1]] = GridState::Sand(SandState::Moving);
-                    }
-                    // falling left out of the area
-                    GridState::Sand(SandState::Moving)
-                        if grid[[x, y + 1]] != GridState::Air && x == 0 =>
-                    {
-                        grid[[x, y]] = GridState::Air;
-                        units -= 1;
-                    }
-                    // falling to the left
-                    GridState::Sand(SandState::Moving)
-                        if grid[[x, y + 1]] != GridState::Air
-                            && grid[[x - 1, y + 1]] == GridState::Air =>
-                    {
-                        grid[[x, y]] = GridState::Air;
-                        grid[[x - 1, y + 1]] = GridState::Sand(SandState::Moving);
-                    }
-                    // falling left out of the area
-                    GridState::Sand(SandState::Moving)
-                        if grid[[x, y + 1]] != GridState::Air && x >= grid_size.0 - 1 =>
-                    {
-                        grid[[x, y]] = GridState::Air;
-                        units -= 1;
-                    }
-                    // falling to the right
-                    GridState::Sand(SandState::Moving)
-                        if grid[[x, y + 1]] != GridState::Air
-                            && grid[[x + 1, y + 1]] == GridState::Air =>
-                    {
-                        grid[[x, y]] = GridState::Air;
-                        grid[[x + 1, y + 1]] = GridState::Sand(SandState::Moving);
-                    }
-                    // finally this sand is not moving anymore
-                    GridState::Sand(SandState::Moving) => {
-                        grid[[x, y]] = GridState::Sand(SandState::Rest);
-                        units_at_rest += 1;
-                    }
-                    _ => {}
+        for (y, x) in iproduct!((0..grid_size.1).rev(), 0..grid_size.0) {
+            // for y in (0..grid_size.1).rev() {
+            //     for x in 0..grid_size.0 {
+            match grid[[x, y]] {
+                // sand falls out of area
+                GridState::Sand(SandState::Moving) if y >= grid_size.1 - 1 => {
+                    grid[[x, y]] = GridState::Air;
+                    units -= 1;
+                    panic!("Sand fell off");
                 }
+                // falling straight down
+                GridState::Sand(SandState::Moving) if grid[[x, y + 1]] == GridState::Air => {
+                    grid[[x, y]] = GridState::Air;
+                    grid[[x, y + 1]] = GridState::Sand(SandState::Moving);
+                }
+                // falling left out of the area
+                GridState::Sand(SandState::Moving)
+                    if grid[[x, y + 1]] != GridState::Air && x == 0 =>
+                {
+                    grid[[x, y]] = GridState::Air;
+                    units -= 1;
+                    panic!("Sand fell off");
+                }
+                // falling to the left
+                GridState::Sand(SandState::Moving)
+                    if grid[[x, y + 1]] != GridState::Air
+                        && grid[[x - 1, y + 1]] == GridState::Air =>
+                {
+                    grid[[x, y]] = GridState::Air;
+                    grid[[x - 1, y + 1]] = GridState::Sand(SandState::Moving);
+                }
+                // falling right out of the area
+                GridState::Sand(SandState::Moving)
+                    if grid[[x, y + 1]] != GridState::Air && x >= grid_size.0 - 1 =>
+                {
+                    grid[[x, y]] = GridState::Air;
+                    units -= 1;
+                    panic!("Sand fell off");
+                }
+                // falling to the right
+                GridState::Sand(SandState::Moving)
+                    if grid[[x, y + 1]] != GridState::Air
+                        && grid[[x + 1, y + 1]] == GridState::Air =>
+                {
+                    grid[[x, y]] = GridState::Air;
+                    grid[[x + 1, y + 1]] = GridState::Sand(SandState::Moving);
+                }
+                // finally this sand is not moving anymore
+                GridState::Sand(SandState::Moving) => {
+                    grid[[x, y]] = GridState::Sand(SandState::Rest);
+                    units_at_rest += 1;
+                }
+                _ => {}
             }
+            // }
         }
         println!("units = {units}  |  units at rest = {units_at_rest}");
-        render_grid(&grid);
+        // render_grid(&grid);
 
         if units == units_before {
+            println!("Units stopped increasing!");
             break;
         }
     }
